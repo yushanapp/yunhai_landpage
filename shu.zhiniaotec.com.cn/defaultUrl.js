63c49bf5caf94a9fb999bbc634a6b64836c1bc0f
Niceone9 <asdcheru5896@qq.com>
2018-08-14 14:41:26 +0800

    shu

--- shu.zhiniaotec.com.cn/defaultUrl.js
+++ shu.zhiniaotec.com.cn/defaultUrl.js
@@ -1,7 +1,23 @@
-var android='http://noapk.apk';
+var android='http://www.hbooker.com/setting/download?channel=shenma';
 var ios='';
 var down=''
 var btndown=''
+
+var url={
+  sem1:'http://www.hbooker.com/setting/download?channel=shenma1',//332
+  sem2:'http://www.hbooker.com/setting/download?channel=shenma2',//334
+  sem3:'http://www.hbooker.com/setting/download?channel=shenma3',//335
+  sem4:'http://www.hbooker.com/setting/download?channel=shenma4',//336
+  sem5:'http://www.hbooker.com/setting/download?channel=shenma5',//337
+  sem6:'http://www.hbooker.com/setting/download?channel=shenma6',//338
+  sem7:'http://www.hbooker.com/setting/download?channel=shenma7',//339
+  sem8:'http://www.hbooker.com/setting/download?channel=shenma8',//587
+};
+
+var c4 = _getQueryString("c4") || "sem1";
+
+android = url[c4];
+
 if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) {
   down='ios'
   btndown=ios;
@@ -12,31 +28,3 @@ if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) {
   down='pc'
   btndown=android;
 };
-
-var url1="//nlp.zntec.mobi/landpageManager/getPackage"+window.location.search;
-$.ajax({
-  type: 'get',
-  url: url1,
-  data:"",
-  dataType:'jsonp',
-  jsonp:'callback',
-  jsonpCallback:"success_jsonp",
-  success: function(data){
-    if(data.status=="failed"){
-
-    }else{
-      if(down=='ios'){
-        btndown=data.data.ios;
-      }else{
-        btndown=data.data.android;
-      }
-
-    }
-
-
-  },
-  error:function(){
-    console.log("ERROR");
-  },
-
-});
