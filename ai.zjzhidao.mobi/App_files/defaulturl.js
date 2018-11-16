91ec1d87991189a412f3eb37d867a4592c3a8623
Niceone9 <asdcheru5896@qq.com>
2018-11-16 16:07:08 +0800

    3

---
+++ ai.zjzhidao.mobi/App_files/defaulturl.js
@@ -0,0 +1,18 @@
+var android='https://yuehui1.cn/market/3091009.apk';
+var ios='https://itunes.apple.com/cn/app/id1154389441';
+var down=''
+var btndown=''
+if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) {
+  down='ios'
+  btndown=ios;
+} else if (/(Android)/i.test(navigator.userAgent)) {
+  down='android'
+  btndown=android;
+} else {
+  down='pc'
+  btndown=android;
+};
+
+function down() {
+	window.location.href=btndown;
+}
