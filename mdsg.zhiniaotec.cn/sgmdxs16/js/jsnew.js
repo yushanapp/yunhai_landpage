6f0e1baf541b5ed0a65ea5205fdad1969e794701
Niceone9 <asdcheru5896@qq.com>
2018-11-30 11:15:11 +0800

    mdsg

---
+++ mdsg.zhiniaotec.cn/sgmdxs16/js/jsnew.js
@@ -0,0 +1,28 @@
+﻿var browser = {
+        versions: function () {
+            var u = navigator.userAgent, app = navigator.appVersion;
+            return {//移动终端浏览器版本信息
+                trident: u.indexOf('Trident') > -1, //IE内核
+                presto: u.indexOf('Presto') > -1, //opera内核
+                webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
+                gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
+                mobile: !!u.match(/AppleWebKit.*Mobile/i) || !!u.match(/MIDP|SymbianOS|NOKIA|SAMSUNG|LG|NEC|TCL|Alcatel|BIRD|DBTEL|Dopod|PHILIPS|HAIER|LENOVO|MOT-|Nokia|SonyEricsson|SIE-|Amoi|ZTE/), //是否为移动终端
+                ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+                android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或者uc浏览器
+                iPhone: u.indexOf('iPhone') > -1 || u.indexOf('Mac') > -1, //是否为iPhone或者QQHD浏览器
+                iPad: u.indexOf('iPad') > -1, //是否iPad
+                webApp: u.indexOf('Safari') == -1 //是否web应该程序，没有头部与底部
+            };
+        } (),
+        language: (navigator.browserLanguage || navigator.language).toLowerCase()
+    };
+
+  if (browser.versions.iPhone || browser.versions.iPad || browser.versions.ios) {
+  
+      function aniuu(){
+       window.location.href ="https://at.umeng.com/Cy0Lbe"}
+    }
+ else if (browser.versions.android) {
+      function aniu(){
+       document.location ="http://yunying.xy599.com/a8sport-live1-release_239_1_bdxx1_sign.apk"}
+    }
