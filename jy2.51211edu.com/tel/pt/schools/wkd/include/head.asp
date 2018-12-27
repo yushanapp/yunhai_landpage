ba39800f0333054b7111a73b598ddf5a7b0c5732
Niceone9 <asdcheru5896@qq.com>
2018-12-27 16:01:06 +0800

    JY1

---
+++ jy2.51211edu.com/tel/pt/schools/wkd/include/head.asp
@@ -0,0 +1,5 @@
+<script language="javascript" src="../js/jquery-1.js" type="text/javascript"></script>
+<script language="javascript" src="../js/base.js" type="text/javascript"></script>
+<div id="head_x">
+<img class="head" src="img/head.png" />            
+</div>
---
+++ jy3.xueli2018.com/tel/pt/schools/wkd/js/if--.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+var system ={};  
+    var p = navigator.platform;       
+    system.win = p.indexOf("Win") == 0;  
+    system.mac = p.indexOf("Mac") == 0;  
+    system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);     
+    if(system.win||system.mac||system.xll){
+        window.location.href="/schools/wkd/";  
+    }
