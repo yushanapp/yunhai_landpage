3dc30d92d587adca036521f4a565b1a93c47a9e7
Niceone9 <asdcheru5896@qq.com>
2019-03-15 16:27:52 +0800

    3

---
+++ tantan.zntec.cc/down1/3.js
@@ -0,0 +1,13 @@
+    var Android="https://tantanapp.com/tantan_BaidusemYunhaiyangfan03.apk";//安卓下载链接
+    var Ios="";//ios下载链接
+
+    var btns = document.getElementsByClassName("btnCilck");
+    for(var i=0;i<btns.length;i++){
+        btns[i].onclick=function () {
+            if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) {
+                window.location.href =Ios;
+            }else {
+                window.location.href =Android;
+            }
+        }
+    };
