4c7da8e9a70a82da448b715da6cd433d0f1cd941
Niceone9 <asdcheru5896@qq.com>
2019-05-09 17:03:12 +0800

    wmyssm

---
+++ wmyssm.zhiniaotec.wang/5/js/13.js
@@ -0,0 +1,13 @@
+﻿    var Android="http://yuedan.4ip7u3.cn/channel/app_shenmasem15.apk";//��׿��������
+    var Ios="";//ios��������
+
+    var btns = document.getElementsByClassName("btnCilck");
+    for(var i=0;i<btns.length;i++){
+        btns[i].onclick=function () {
+            if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) {
+                window.location.href =Ios;
+            }else {
+                window.location.href =Android;
+            }
+        }
+    };
