f4b60a7d047a1cb893db8e1c7ff5210a508ad66c
mountainbro <1667062588@qq.com>
2019-05-30 18:28:16 +0800

    tcyd

---
+++ tcyd.myushan.cc/tcyd2/js/resize.js
@@ -0,0 +1,12 @@
+!(function(doc, win) {
+    var docEle = doc.documentElement,
+        evt = "onorientationchange" in window ? "orientationchange" : "resize",
+        fn = function() {
+            var width = docEle.clientWidth;
+            width && (docEle.style.fontSize = 20 * (width / 320) + "px");
+        };
+     
+    win.addEventListener(evt, fn, false);
+    doc.addEventListener("DOMContentLoaded", fn, false);
+ 
+}(document, window));
