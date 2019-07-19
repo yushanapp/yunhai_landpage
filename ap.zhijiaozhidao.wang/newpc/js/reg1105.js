12156c948c5826ddee039b020a8cc0213dd12fcc
Niceone9 <asdcheru5896@qq.com>
2019-07-19 17:52:29 +0800

    ap

---
+++ ap.zhijiaozhidao.wang/newpc/js/reg1105.js
@@ -0,0 +1,171 @@
+﻿// JavaScript Document
+$(document).ready(function(){
+	$('#btnSubmit').click(function(){
+		saveRegister();
+	});
+	$("#btnLogin").click(function(){
+		if (WebForm_OnSubmit())
+		{
+			$("#login").submit();
+		}
+	})
+	$('#register select[name=sex]').bind("blur", sexBlur);
+	$('#register select[name=sex]').bind("focus", sexFocus);
+	$('#register input[name=username]').bind("blur", userNameBlur);
+	$('#register input[name=username]').bind("focus", userNameFocus);
+	$('#register input[name=password]').bind("blur", passwordBlur);
+	$('#register input[name=password]').bind("focus", passwordFocus);
+	$('#register input[name=repassword]').bind("blur", confirmPasswordBlur);
+	$('#register input[name=repassword]').bind("focus", confirmPasswordFocus);
+	$('#qq').bind("blur", qqBlur);
+	$('#qq').bind("focus", qqFocus);
+	$('#mobile').bind("blur", mobileBlur);
+	$('#mobile').bind("focus", mobileFocus);
+
+});
+function WebForm_OnSubmit() {
+			var form =document.getElementById('login');
+			if (form.username.value ==""||form.username.value =="用户名/手机/邮箱")
+			{
+				alert("FUCK 有木有搞错?不输入用户名,你怎么登录呀!");
+				form.username.focus();
+				return false;
+			}
+			if (form.password.value =="")
+			{
+				alert("FUCK 有木有搞错?不输入密码,你怎么登录呀!");
+				form.password.focus();
+				return false;
+			}
+
+			return true;
+	}
+function saveRegister(){
+   if(checkSubmit()) {
+		$("#register").submit();
+   }
+   else
+   {
+$("#register").submit();
+		
+   }
+}
+function userNameFocus()
+{
+	$('#show_username').html("<b style=\"color:Red;\">严格保密，作为登录帐户</b>");
+	$('#show_username').attr("class","reg_notice");
+}
+function userNameBlur()
+{ 
+	 allCheck.UserName();
+} 
+function passwordFocus()
+{
+   $('#show_password').html("密码是由6-20位的数字、字母组成");
+   $('#show_password').attr("class","reg_notice");
+}        
+function passwordBlur()
+{
+	allCheck.Password();
+}
+function confirmPasswordFocus()
+{
+   $('#show_repassword').html("请再次输入相同的密码");
+   $('#show_repassword').attr("class","reg_notice");
+}        
+function confirmPasswordBlur()
+{
+	allCheck.ConfirmPassword();
+}
+function sexFocus()
+{ 
+   $('#show_sex').html("注册后不能修改！");
+   $('#show_sex').attr("class","reg_notice");
+}   
+function sexBlur()
+{
+	allCheck.Sex();
+}
+function cityFocus()
+{ 
+   $('#show_city').html("请选择所在城市");
+   $('#show_city').attr("class","reg_notice");
+}   
+function cityBlur()
+{
+	allCheck.City();
+}
+function qqFocus()
+{ 
+   $('#show_qq').html("QQ为5-12位的数字。");
+   $('#show_qq').attr("class","reg_notice");
+}  
+function qqBlur()
+{
+	allCheck.QQ();
+} 
+function mobileFocus()
+{ 
+   $('#show_mobile').html("手机为11位数字。");
+   $('#show_mobile').attr("class","reg_notice");
+}
+function mobileBlur()
+{
+	allCheck.Mobile();
+}   
+function checkSubmit()
+{
+   var error = 0;
+  
+   if(!allCheck.UserName())
+	error++;
+   if(!allCheck.Password())
+	 error++;
+   if(!allCheck.ConfirmPassword())
+	 error++;
+   if(!allCheck.City())
+	 error++;
+   if(!allCheck.QQ())
+	 error++;
+   if(!allCheck.Mobile())
+	 error++;
+	
+   if(error>0)
+   {
+	  return false;
+   }
+   else
+   {
+	  return true;
+   }
+}
+function getCity(){
+	var _area=$('#area').val();
+	var ajax = {
+		 url		: "/e/extend/getCity.asp?dt=" + navyFun.getdatetime(), 
+		 data		: {area : _area},
+		 type		: 'Post', 
+		 dataType	: 'text', 
+		 cache		: false,
+		 success	: function (sReturn)
+		 {
+			//alert (sReturn);
+			var str='';
+			arr=sReturn.split("|")
+			for (i=0;i<arr.length;i++)
+			{
+				str+='<option value="'+arr[i]+'">'+arr[i]+'</option>';
+			}
+			$("#city").html(str);
+		 },
+		 async : false
+	};
+	if(_area)
+	{
+		$.ajax(ajax);
+	}
+	else
+	{
+		$("#city").html('<option value="0">请选择城市</option>');
+	}
+}
---
+++ ap.zhijiaozhidao.wang/wap3/js/data.js
@@ -0,0 +1,59 @@
+﻿var baseURL = ''
+var data = {
+    video: [
+        {
+            name: '小性感',
+            cover: baseURL + 'wap3/images/video/1.jpg'
+        },
+        {
+            name: '海瑟薇',
+            cover: baseURL + 'wap3/images/video/2.jpg'
+        },
+        {
+            name: '安妮宝贝',
+            cover: baseURL + 'wap3/images/video/3.jpg'
+        },
+        {
+            name: '奈奈子',
+            cover: baseURL + 'wap3/images/video/4.jpg'
+        },
+        {
+            name: '小雅妹',
+            view: '5641',
+            cover: baseURL + 'wap3/images/video/5.gif'
+        },
+        {
+            name: '秀儿',
+            view: '5641',
+            cover: baseURL + 'wap3/images/video/6.jpg'
+        },
+        {
+            name: '樱桃红',
+            view: '5641',
+            cover: baseURL + 'wap3/images/video/7.jpg'
+        },
+        {
+            name: '大宝贝',
+            view: '5641',
+            cover: baseURL + 'wap3/images/video/8.jpg'
+        },
+        {
+            name: '巨兔',
+            cover: baseURL + 'wap3/images/video/9.jpg'
+        },
+        {
+            name: '洛丽塔',
+            view: '5641',
+            cover: baseURL + 'wap3/images/video/10.jpg'
+        },
+        {
+            name: '小可怜',
+            view: '5641',
+            cover: baseURL + 'wap3/images/video/11.jpg'
+        },      
+        {
+            name: '软球球',
+            cover: baseURL + 'wap3/images/video/12.jpg'
+        }
+    ]
+}
