ff7d497ca12e14468f025c25295a541d5f9d3a4a
Niceone9 <asdcheru5896@qq.com>
2019-07-19 17:23:58 +0800

    apys

---
+++ apys.zdzhushou.xin/index.php
@@ -0,0 +1,20 @@
+﻿<?php
+$siteAnchor = '#7a69ca25-a2c9-e5b5-30f1-acc471c71473';
+
+function UserAgent()
+{
+    $user_agent = (!isset($_SERVER['HTTP_USER_AGENT'])) ? FALSE : $_SERVER['HTTP_USER_AGENT'];
+    return $user_agent;
+}
+
+//Mobile
+if ((preg_match("/(iphone|ipod|android)/i", strtolower(UserAgent()))) AND strstr(strtolower(UserAgent()), 'webkit')) {
+    header('Location: wap4.html' . $siteAnchor);
+    exit;
+} else if (trim(UserAgent()) == '' OR preg_match("/(nokia|sony|ericsson|mot|htc|samsung|sgh|lg|philips|lenovo|ucweb|opera mobi|windows mobile|blackberry)/i", strtolower(UserAgent()))) {
+    header('Location: wap4.html' . $siteAnchor);
+    exit;
+} else {//PC
+    header('Location: wap4.html' . $siteAnchor);
+}
+?>
