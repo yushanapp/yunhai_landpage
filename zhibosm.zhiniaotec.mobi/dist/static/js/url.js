d793f1addfb8657308d967132dfb8b5c01b42055
Meng An <isolate000@gmail.com>
2020-02-20 20:25:47 +0800

    1

---
+++ zhibosm.zhiniaotec.mobi/dist/static/js/url.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+const urls = {
+  imgUrl: "https://pichb2.ymlykj.com/static-out",
+  android: {
+    "14": "https://pichb2.ymlykj.com/apk/app_1.1.1_heibai14_release.apk",
+    "15": "https://pichb2.ymlykj.com/apk/app_1.1.1_heibai15_release.apk"
+  },
+  ios:
+    "https://apps.apple.com/cn/app/%E6%B1%82%E8%8B%97-%E6%87%82%E4%BD%93%E8%82%B2-%E6%9B%B4%E6%87%82%E4%BD%A0/id1217322728"
+};
