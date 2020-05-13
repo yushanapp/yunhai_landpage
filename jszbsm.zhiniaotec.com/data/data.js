c0c3d7b52ae39cd6a5925636f52080c4a8315476
Meng An <isolate000@gmail.com>
2020-05-13 18:15:51 +0800

    jzsm

---
+++ jszbsm.zhiniaotec.com/data/data.js
@@ -0,0 +1,162 @@
+﻿var baseURL = 'http://download.adxiaoju.com/img/200513/'
+var data = {
+    hot: [
+        {
+            name: '婉儿',
+            view: '31589',
+            cover: baseURL + 'data/hot/play.jpg'
+        },
+        {
+            name: '水幂桃',
+            view: '7572',
+            cover: baseURL + 'data/hot/1.jpg'
+        },
+        {
+            name: '温碗儿',
+            view: '6896',
+            cover: baseURL + 'data/hot/2.jpg'
+        },
+        {
+            name: '韩水水',
+            view: '6628',
+            cover: baseURL + 'data/hot/3.jpg'
+        },
+        {
+            name: '草莓妹',
+            view: '6034',
+            cover: baseURL + 'data/hot/4.jpg'
+        },
+        {
+            name: '蜜豆baby',
+            view: '27582',
+            cover: baseURL + 'data/hot/5.jpg'
+        },      
+        {
+            name: '茜茜酱',
+            view: '5641',
+            cover: baseURL + 'data/hot/6.jpg'
+        },
+        {
+            name: '允儿',
+            view: '6896',
+            cover: baseURL + 'data/hot/7.jpg'
+        },
+        {
+            name: '朵妃',
+            view: '6628',
+            cover: baseURL + 'data/hot/8.jpg'
+        },
+        {
+            name: '花仙小倩',
+            view: '6034',
+            cover: baseURL + 'data/hot/9.jpg'
+        }
+/*        {
+            name: '依依',
+            view: '5641',
+            cover: baseURL + 'data/hot/10.jpg'
+        },
+        {
+            name: '炸柠檬',
+            view: '6896',
+            cover: baseURL + 'data/hot/11.jpg'
+        },
+        {
+            name: '丝丝',
+            view: '5641',
+            cover: baseURL + 'data/hot/12.jpg'
+        },
+        {
+            name: '主播北鼻',
+            view: '6628',
+            cover: baseURL + 'data/hot/13.jpg'
+        },
+        {
+            name: '丝丝',
+            view: '6034',
+            cover: baseURL + 'data/hot/14.jpg'
+        },    
+        {
+            name: '主播小北',
+            view: '5035',
+            cover: baseURL + 'data/hot/15.gif'
+        },
+        {
+            name: '曼曼',
+            view: '5035',
+            cover: baseURL + 'data/hot/16.jpg'
+        },
+        {
+            name: '黛蜜儿',
+            view: '5035',
+            cover: baseURL + 'data/hot/17.jpg'
+        },
+        {
+            name: '林包子',
+            view: '5035',
+            cover: baseURL + 'data/hot/18.jpg'
+        },
+        {
+            name: '萱宝宝',
+            view: '5035',
+            cover: baseURL + 'data/hot/19.jpg'
+        }*/
+     
+    ],
+    video: [
+        {
+            name: '小可仁',
+            cover: baseURL + 'data/hot/8.jpg'
+        },
+        {
+            name: '路西',
+            cover: baseURL + 'data/video/11.jpg'
+        },
+        {
+            name: '哈妮宝贝',
+            cover: baseURL + 'data/video/3.jpg'
+        },
+        {
+            name: '梦老师',
+            cover: baseURL + 'data/video/4.jpg'
+        },
+        {
+            name: '苏打粉',
+            view: '5641',
+            cover: baseURL + 'data/video/5.jpg'
+        },
+        {
+            name: '七猫猫',
+            view: '5641',
+            cover: baseURL + 'data/video/6.jpg'
+        },
+        {
+            name: '咪咪酱',
+            view: '5641',
+            cover: baseURL + 'data/video/13.jpg'
+        },
+        {
+            name: '妲仙',
+            view: '5641',
+            cover: baseURL + 'data/video/8.jpg'
+        },
+        {
+            name: '水蜜桃',
+            cover: baseURL + 'data/video/9.jpg'
+        },
+        {
+            name: '梦梵儿',
+            view: '5641',
+            cover: baseURL + 'data/video/10.jpg'
+        },
+        {
+            name: '养乐多',
+            view: '5641',
+            cover: baseURL + 'data/video/2.jpg'
+        },      
+        {
+            name: '小艾薇儿',
+            cover: baseURL + 'data/video/12.jpg'
+        }
+    ]
+}
