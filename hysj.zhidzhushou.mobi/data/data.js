c00a58ccea4cb64156c1b8305bbbf8c9e5e49212
Meng An <isolate000@gmail.com>
2020-06-23 11:36:42 +0800

    hysj

---
+++ hysj.zhidzhushou.mobi/data/data.js
@@ -0,0 +1,194 @@
+﻿var baseURL = 'https://rec.net678.vip/hysj.zhidzhushou.mobi/'
+var data = {
+    hot: [
+        {
+            name: '尹素婉',
+            view: '31589',
+            cover: baseURL + 'data/hot/play.gif',
+            city:'杭州'
+        },
+        {
+            name: '水幂桃',
+            view: '7572',
+            cover: baseURL + 'data/hot/1.jpg',
+            city:'杭州'
+        },
+        {
+            name: '温碗儿',
+            view: '6896',
+            cover: baseURL + 'data/hot/2.jpg',
+            city:'杭州'
+        },
+        {
+            name: '韩水水',
+            view: '6628',
+            cover: baseURL + 'data/hot/3.jpg',
+            city:'杭州'
+        },
+        {
+            name: '草莓妹',
+            view: '6034',
+            cover: baseURL + 'data/hot/4.gif',
+            city:'杭州'
+        },
+        {
+            name: '蜜豆baby',
+            view: '27582',
+            cover: baseURL + 'data/hot/5.jpg',
+            city:'杭州'
+        },      
+        {
+            name: '茜茜酱',
+            view: '5641',
+            cover: baseURL + 'data/hot/6.jpg',
+            city:'杭州'
+        },
+        {
+            name: '允儿',
+            view: '6896',
+            cover: baseURL + 'data/hot/7.jpg',
+            city:'杭州'
+        },
+        {
+            name: '朵妃',
+            view: '6628',
+            cover: baseURL + 'data/hot/8.jpg',
+            city:'杭州'
+        },
+        {
+            name: '花仙小倩',
+            view: '6034',
+            cover: baseURL + 'data/hot/9.jpg',
+            city:'杭州'
+        },
+        {
+            name: '依依',
+            view: '5641',
+            cover: baseURL + 'data/hot/10.jpg',
+            city:'杭州'
+        },
+        {
+            name: '炸柠檬',
+            view: '6896',
+            cover: baseURL + 'data/hot/11.jpg',
+            city:'杭州'
+        },
+        {
+            name: '丝丝',
+            view: '5641',
+            cover: baseURL + 'data/hot/12.jpg',
+            city:'杭州'
+        },
+        {
+            name: '主播北鼻',
+            view: '6628',
+            cover: baseURL + 'data/hot/13.jpg',
+            city:'杭州'
+        },
+        {
+            name: '丝丝',
+            view: '6034',
+            cover: baseURL + 'data/hot/14.jpg',
+            city:'杭州'
+        },    
+        {
+            name: '主播小北',
+            view: '5035',
+            cover: baseURL + 'data/hot/15.gif',
+            city:'杭州'
+        },
+        {
+            name: '曼曼',
+            view: '5035',
+            cover: baseURL + 'data/hot/16.jpg',
+            city:'杭州'
+        },
+        {
+            name: '黛蜜儿',
+            view: '5035',
+            cover: baseURL + 'data/hot/17.jpg',
+            city:'杭州'
+        },
+        {
+            name: '林包子',
+            view: '5035',
+            cover: baseURL + 'data/hot/18.jpg',
+            city:'杭州'
+        },
+        {
+            name: '萱宝宝',
+            view: '5035',
+            cover: baseURL + 'data/hot/19.jpg',
+            city:'杭州'
+        }
+     
+    ],
+    video: [
+        {
+            name: '曦嘻吸熙',
+            cover: baseURL + 'data/video/1.jpg',
+            city:'杭州'
+        },
+        {
+            name: '路西',
+            cover: baseURL + 'data/video/2.jpg',
+            city:'杭州'
+        },
+        {
+            name: '哈妮宝贝',
+            cover: baseURL + 'data/video/3.jpg',
+            city:'杭州'
+        },
+        {
+            name: '梦老师',
+            cover: baseURL + 'data/video/4.jpg',
+            city:'杭州'
+        },
+        {
+            name: '艾玛寒亚',
+            view: '5641',
+            cover: baseURL + 'data/video/5.jpg',
+            city:'杭州'
+        },
+        {
+            name: '七猫猫',
+            view: '5641',
+            cover: baseURL + 'data/video/6.jpg',
+            city:'杭州'
+        },
+        {
+            name: '咪咪酱',
+            view: '5641',
+            cover: baseURL + 'data/video/13.jpg',
+            city:'杭州'
+        },
+        {
+            name: '妲仙',
+            view: '5641',
+            cover: baseURL + 'data/video/8.jpg',
+            city:'杭州'
+        },
+        {
+            name: '水蜜桃',
+            cover: baseURL + 'data/video/9.jpg',
+            city:'杭州'
+        },
+        {
+            name: '梦梵儿',
+            view: '5641',
+            cover: baseURL + 'data/video/10.jpg',
+            city:'杭州'
+        },
+        {
+            name: '养乐多',
+            view: '5641',
+            cover: baseURL + 'data/video/11.jpg',
+            city:'杭州'
+        },      
+        {
+            name: '艾菲尔',
+            cover: baseURL + 'data/video/12.jpg',
+            city:'杭州'
+        }
+    ]
+}
